78977511b3e5a160c3b96ef415761c05b36876cd
simon-kloker <simon.kloker@mail.de>
2026-05-12 17:29:15 +0200

    Add function to format worker names

---
+++ session-10/worker_names.py
@@ -0,0 +1,7 @@
+def get_formatted_worker_name(first, last):
+    """
+    Return a neatly formatted worker name.
+    Example: Henry Dunant
+    """
+    full_name = f"{first} {last}"
+    return full_name.title()
